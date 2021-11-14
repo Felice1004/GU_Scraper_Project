c772428254fa42b5ed8f45bd478eb48ffe40bfa5
DESKTOP-86UIN97\felic <palette1004@gmail.com>
2021-11-15 01:07:04 +0800

    item pages added/ deployment

---
+++ GU_OUTLET/resources/itemDetailView.py
@@ -0,0 +1,48 @@
+from flask_restful import Resource 
+from config import config
+from flask import Flask, render_template, make_response
+import psycopg2
+
+class ItemDetailView(Resource):
+    def get(self, category, id):
+        categories = ['coat', 'jacket','parka','cardigan']
+        if category not in categories:
+            return make_response('page not found')
+        else:
+            sql = "SELECT * FROM ITEM WHERE itemid = " + "'" + id + "'"
+
+            conn = None
+            try:
+                params = config()
+                conn = psycopg2.connect(**params)
+                cur = conn.cursor()
+                cur.execute(sql)
+                result = cur.fetchall()
+
+                results = []
+
+                for item in result:
+                    result = []
+                    result.append(item[0]) #id
+                    result.append(item[1]) #uid
+                    result.append(item[2]) #title
+                    result.append('https://www.gu-global.com/tw/hmall/test/'+ item[1] +'/main/first/561/1.jpg')
+                    result.append(item[-1]) # colorid
+                    results.append(result)
+                cur.close()
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+            finally:
+                if conn is not None:
+                    conn.close()
+            # return render_template(category+'.html', results = results)
+            return make_response(render_template('itemDetail.html',results = results),200)
+
+    def post(self, name):
+        pass
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        pass
